57049b775fcd14440260d1df45b438bff8306e85
8utc6er_Hu <sshx1997@gmail.com>
2025-03-05 23:33:57 +0800

    debug: Add some print for debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,18 @@ svlint --version
 printf 'SVLINT_CONFIG=%s\n' ${SVLINT_CONFIG}
 
 files=$1
-
+filelists=""
 for filelist in $2
 do
     filelists="$filelists -f $filelist"
 done
+
+args=""
+if [ -n "$filelists" ]; then
+    args="$filelists"
+else
+    args="$files"
+fi
 
 ruleset=$3
 if [ -z "$3" ]
@@ -17,5 +24,5 @@ then
 else
   svlint="svlint-$ruleset"
 fi
-
-$svlint --github-actions $files $filelists
+printf 'svlint command: %s --github-actions %s\n' $svlint $args
+$svlint --github-actions $args
